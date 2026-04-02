a7b3d847042e6668ec3b056360d8a52a254bd159
pingusus <lorenzorosca09@gmail.com>
2026-04-02 19:42:30 +0200

    Update ScoreboardListener.java

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/ScoreboardListener.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/ScoreboardListener.java
@@ -1,108 +1,92 @@
-/*
- * BedWars2023 - A bed wars mini-game.
- * Copyright (C) 2024 Tomas Keuper
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * Contact e-mail: contact@fyreblox.com
- */
-
 package com.tomkeuper.bedwars.sidebar;
 
-import com.tomkeuper.bedwars.BedWars;
+import com.tomkeuper.bedwars.support.TabSupport;
 import com.tomkeuper.bedwars.api.arena.IArena;
-import com.tomkeuper.bedwars.api.configuration.ConfigPath;
-import com.tomkeuper.bedwars.api.events.player.PlayerBedBreakEvent;
-import com.tomkeuper.bedwars.api.events.player.PlayerKillEvent;
-import com.tomkeuper.bedwars.api.events.player.PlayerReJoinEvent;
-import com.tomkeuper.bedwars.api.events.player.PlayerReSpawnEvent;
-import com.tomkeuper.bedwars.arena.Arena;
+import com.tomkeuper.bedwars.api.arena.team.ITeam;
+import com.tomkeuper.bedwars.api.language.Language;
+import com.tomkeuper.bedwars.api.language.Messages;
+import com.tomkeuper.bedwars.events.player.PlayerJoinArenaEvent;
+import com.tomkeuper.bedwars.events.player.PlayerLeaveArenaEvent;
+import com.tomkeuper.bedwars.events.player.PlayerReSpawnEvent;
+import com.tomkeuper.bedwars.support.TabSupport;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityRegainHealthEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ScoreboardListener implements Listener {
-    // Leaving this here if needed for possible health integrations. (27/4/2023)
-    // Listener is not registered!
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerDamage(EntityDamageEvent e) {
-        if (e == null) return;
-        if (e.isCancelled()) return;
-        if (!(e.getEntity() instanceof Player)) return;
-        final Player player = (Player) e.getEntity();
-        final IArena arena = Arena.getArenaByPlayer(player);
-
-        int health = (int) Math.ceil((player.getHealth() - e.getFinalDamage()));
-        if (arena == null) return;
-
-//        SidebarService.getInstance().refreshHealth(arena, player, health);
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onRegain(EntityRegainHealthEvent e) {
-        if (e == null) return;
-        if (e.isCancelled()) return;
-        if (!(e.getEntity() instanceof Player)) return;
-        final Player player = (Player) e.getEntity();
-        final IArena arena = Arena.getArenaByPlayer(player);
-        if (arena == null) return;
-
-        int health = (int) Math.ceil(player.getHealth() + e.getAmount());
-
-//        SidebarService.getInstance().refreshHealth(arena, player, health);
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onReSpawn(PlayerReSpawnEvent e) {
-        if (e == null) return;
-        final IArena arena = e.getArena();
-
-//        SidebarService.getInstance().refreshHealth(arena, e.getPlayer(), (int) Math.ceil(e.getPlayer().getHealth()));
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void reJoin(PlayerReJoinEvent e) {
-        if (e == null) return;
-        if (e.isCancelled()) return;
-        if (!BedWars.config.getBoolean(ConfigPath.SB_CONFIG_SIDEBAR_LIST_FORMAT_PLAYING)) return;
-        final IArena arena = e.getArena();
-        final Player player = e.getPlayer();
-
-        // re-add player to scoreboard tab list
-//        SidebarService.getInstance().handleReJoin(arena, player);
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        IArena a = com.tomkeuper.bedwars.arena.Arena.getArenaByPlayer(p);
+        
+        String header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_LOBBY);
+        String footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_LOBBY);
+        
+        if (a != null) {
+            switch (a.getStatus()) {
+                case playing:
+                    header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_PLAYING);
+                    footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_PLAYING);
+                    break;
+                case waiting:
+                    header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_WAITING);
+                    footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_WAITING);
+                    break;
+                case starting:
+                    header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_STARTING);
+                    footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_STARTING);
+                    break;
+            }
+        }
+        
+        TabSupport.setHeaderFooter(p, header, footer);
     }
 
     @EventHandler
-    public void onBedDestroy(PlayerBedBreakEvent e) {
-        if (e == null) return;
-        final IArena arena = e.getArena();
-
-        // refresh placeholders in case placeholders refresh is disabled
-//        SidebarService.getInstance().refreshPlaceholders(arena);
+    public void onLeave(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        TabSupport.removeTabPrefix(p);
     }
 
     @EventHandler
-    public void onFinalKill(PlayerKillEvent e) {
-        if (e == null) return;
-        if (!e.getCause().isFinalKill()) return;
-        final IArena arena = e.getArena();
+    public void onJoinArena(PlayerJoinArenaEvent e) {
+        Player p = e.getPlayer();
+        IArena a = e.getArena();
+        
+        String header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_WAITING);
+        String footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_WAITING);
+        
+        TabSupport.setHeaderFooter(p, header, footer);
+        
+        ITeam t = a.getTeam(p);
+        if (t != null) {
+            String prefix = t.getColor().chat() + "";
+            String suffix = "";
+            TabSupport.setTabPrefix(p, prefix, suffix);
+        }
+    }
 
-        // refresh placeholders in case placeholders refresh is disabled
-//        SidebarService.getInstance().refreshPlaceholders(arena);
+    @EventHandler
+    public void onLeaveArena(PlayerLeaveArenaEvent e) {
+        Player p = e.getPlayer();
+        TabSupport.removeTabPrefix(p);
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerReSpawnEvent e) {
+        Player p = e.getPlayer();
+        IArena a = e.getArena();
+        ITeam t = a.getTeam(p);
+        
+        if (t != null) {
+            String prefix = t.getColor().chat() + "";
+            String suffix = "";
+            TabSupport.setTabPrefix(p, prefix, suffix);
+        }
     }
 }
