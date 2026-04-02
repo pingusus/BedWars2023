90509c943af0f94a977e0d3cba10515d02755be8
pingusus <lorenzorosca09@gmail.com>
2026-04-02 20:14:55 +0200

    Update BoardListener.java

--- bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/BoardListener.java
+++ bedwars-plugin/src/main/java/com/tomkeuper/bedwars/sidebar/BoardListener.java
@@ -1,53 +1,71 @@
+/*
+ * BedWars2023 - A bed wars mini-game.
+ * Copyright (C) 2024 Tomas Keuper
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact e-mail: contact@fyreblox.com
+ */
+
 package com.tomkeuper.bedwars.sidebar;
 
-import com.tomkeuper.bedwars.support.TabSupport;
 import com.tomkeuper.bedwars.api.arena.IArena;
-import com.tomkeuper.bedwars.api.language.Language;
-import com.tomkeuper.bedwars.api.language.Messages;
-import com.tomkeuper.bedwars.support.TabSupport;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import com.tomkeuper.bedwars.api.events.player.PlayerLeaveArenaEvent;
+import com.tomkeuper.bedwars.arena.Arena;
+import me.neznamy.tab.api.TabAPI;
+import me.neznamy.tab.api.TabPlayer;
+import me.neznamy.tab.api.bossbar.BossBar;
+import me.neznamy.tab.api.placeholder.PlayerPlaceholder;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.util.Objects;
 
 public class BoardListener implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-        IArena a = com.tomkeuper.bedwars.arena.Arena.getArenaByPlayer(p);
-        
-        String header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_LOBBY);
-        String footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_LOBBY);
-        
-        if (a != null) {
-            switch (a.getStatus()) {
-                case playing:
-                    header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_PLAYING);
-                    footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_PLAYING);
-                    break;
-                case waiting:
-                    header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_WAITING);
-                    footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_WAITING);
-                    break;
-                case starting:
-                    header = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_HEADER_STARTING);
-                    footer = Language.getMsg(p, Messages.FORMATTING_SCOREBOARD_TAB_FOOTER_STARTING);
-                    break;
+    public void onArenaLeave(PlayerLeaveArenaEvent event){
+        IArena arena = Arena.getArenaByPlayer(event.getPlayer());
+        if (TabAPI.getInstance().getPlayer(event.getPlayer().getUniqueId()) == null) return;
+        if (TabAPI.getInstance().getBossBarManager() != null && arena != null){
+            for (BossBar bossBar : arena.getDragonBossbars()){
+                bossBar.removePlayer(Objects.requireNonNull(TabAPI.getInstance().getPlayer(event.getPlayer().getUniqueId())));
             }
         }
-        
-        // Add import at top of file when editing:
-        // import com.tomkeuper.bedwars.support.TabSupport;
-        
-        TabSupport.setHeaderFooter(p, header, footer);
+
+        // Force update the prefix and suffix
+        PlayerPlaceholder prefixPlaceholderTab = (PlayerPlaceholder) TabAPI.getInstance().getPlaceholderManager().getPlaceholder("%bw_prefix_tab%");
+        PlayerPlaceholder suffixPlaceholderTab = (PlayerPlaceholder) TabAPI.getInstance().getPlaceholderManager().getPlaceholder("%bw_suffix_tab%");
+        PlayerPlaceholder prefixPlaceholderHead = (PlayerPlaceholder) TabAPI.getInstance().getPlaceholderManager().getPlaceholder("%bw_prefix_head%");
+        PlayerPlaceholder suffixPlaceholderHead = (PlayerPlaceholder) TabAPI.getInstance().getPlaceholderManager().getPlaceholder("%bw_suffix_head%");
+        TabPlayer tabPlayer = TabAPI.getInstance().getPlayer(event.getPlayer().getUniqueId());
+
+        assert tabPlayer != null;
+        prefixPlaceholderTab.updateValue(tabPlayer, BoardManager.getInstance().getPrefixTab(tabPlayer));
+        suffixPlaceholderTab.updateValue(tabPlayer, BoardManager.getInstance().getSuffixTab(tabPlayer));
+        prefixPlaceholderHead.updateValue(tabPlayer, BoardManager.getInstance().getPrefixHead(tabPlayer));
+        suffixPlaceholderHead.updateValue(tabPlayer, BoardManager.getInstance().getSuffixHead(tabPlayer));
     }
 
     @EventHandler
-    public void onLeave(PlayerQuitEvent e) {
-        Player p = e.getPlayer();
-        TabSupport.removeTabPrefix(p);
+    public void onDisconnect(PlayerLeaveArenaEvent event) {
+        BoardManager.getInstance().cleanupPlayer(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event){
+        BoardManager.getInstance().tabPlayerCache.remove(event.getPlayer().getUniqueId());
     }
 }
